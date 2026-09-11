150e8199c813d729c730818b2fb74b4e1c9f1e22
John White <galuchaw@gmail.com>
2026-09-11 18:04:09 -0400

    Release BEAT Engine 0.1.2

--- src/beat_engine/__init__.py
+++ src/beat_engine/__init__.py
@@ -3,5 +3,5 @@
 from .client import EngineWorker, WorkerPool
 from .paths import EnginePaths, engine_paths
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __all__ = ["EnginePaths", "EngineWorker", "WorkerPool", "engine_paths"]
